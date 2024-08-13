36e998c23c4b9f3807d1bf5f52c729d963d0f2f4
Daniel Martins <email.to.x@gmail.com>
2024-08-13 16:05:37 +0100

    Update weather_consumer.py

--- weather_consumer.py
+++ weather_consumer.py
@@ -14,26 +14,29 @@ def get_weather(api_key, city):
 		wind_speed = data['wind']['speed'] 
 		wind_deg = data['wind']['deg']
 		wind_kmph = round(wind_speed * 3.6 , 1)
-		print("temperature", temperature, "city", city, "k", api_key)
+		print("temperature", temperature, "city", city)
 		return weather_description, temperature, temp_min, temp_max, wind_speed, wind_deg, wind_kmph
 	else:
 		raise Exception(f"Failed to get weather data: {response.status_code}")
 
 def main():
 	api_key = os.getenv('OPENWEATHER_API_KEY')
-	city = os.getenv('OPENWEATHER_CITY')
 	if api_key:
-		try:
-			weather_description, temperature,  temp_min, temp_max, wind_speed, wind_deg, wind_kmph = get_weather(api_key, city)
-			print(f"::set-output name=weather_description::{weather_description}")
-			print(f"::set-output name=temperature::{temperature}")
-			print(f"::set-output name=temp_min::{temp_min}")
-			print(f"::set-output name=temp_max::{temp_max}")
-			print(f"::set-output name=wind_speed::{wind_speed}")
-			print(f"::set-output name=wind_kmph::{wind_kmph}")
-			print(f"::set-output name=wind_deg::{wind_deg}")
-		except Exception as e:
-			print(f"::error::{str(e)}")
+		city = os.getenv('OPENWEATHER_API_CITY')
+		if city:
+			try:
+				weather_description, temperature,  temp_min, temp_max, wind_speed, wind_deg, wind_kmph = get_weather(api_key, city)
+				print(f"::set-output name=weather_description::{weather_description}")
+				print(f"::set-output name=temperature::{temperature}")
+				print(f"::set-output name=temp_min::{temp_min}")
+				print(f"::set-output name=temp_max::{temp_max}")
+				print(f"::set-output name=wind_speed::{wind_speed}")
+				print(f"::set-output name=wind_kmph::{wind_kmph}")
+				print(f"::set-output name=wind_deg::{wind_deg}")
+			except Exception as e:
+				print(f"::error::{str(e)}")
+		else:
+			print("::error::Please set the OPENWEATHER_API_CITY environment variable.")
 	else:
 		print("::error::Please set the OPENWEATHER_API_KEY environment variable.")
 
