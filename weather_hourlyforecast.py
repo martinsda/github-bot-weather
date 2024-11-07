535984b55f4c1a7105a580f7715b7d34993cdf8a
Daniel Martins <email.to.x@gmail.com>
2024-11-07 17:37:11 +0000

    Update weather_hourlyforecast.py

--- weather_hourlyforecast.py
+++ weather_hourlyforecast.py
@@ -11,7 +11,7 @@ import json
 def writejson_data(data):
     json_string = json.dumps(data, indent=4)
     with open('weatherdata.json', 'w') as json_file:
-    	json_file.write(json_string)
+    	json_file.write(str(json_string))
 
 	    
 def get_forecast_weather(lat, long):
