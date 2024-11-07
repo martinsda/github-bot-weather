6c1e53fd36f4ab5be6549a9cefe1e2dfa0761c2d
Daniel Martins <email.to.x@gmail.com>
2024-11-07 17:13:10 +0000

    Update weather_hourlyforecast.py

--- weather_hourlyforecast.py
+++ weather_hourlyforecast.py
@@ -57,7 +57,7 @@ def get_forecast_weather(lat, long):
   hourly_dataframe = pd.DataFrame(data = hourly_data)
   json_data = {
     "name": "John Doe",
-    "age": 30,
+    "age": 31,
     "city": "New York",
     "skills": ["Python", "Data Analysis", "Machine Learning"]}
   writejson_data(json_data)
