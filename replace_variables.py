c5d457a0bb4396133f97e970cc6df303505c6424
Daniel Martins <email.to.x@gmail.com>
2024-11-13 19:27:12 +0000

    Update replace_variables.py

--- replace_variables.py
+++ replace_variables.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     template_path = os.getenv('TEMPLATE_PATH')
     output_path = os.getenv('OUTPUT_PATH')
     code =  os.getenv('WEATHER_CODE')
-    if now.hour >= 20:
+    if now.hour >= 19:
         weather_info = get_weather_info(code,'night') 
     else:
         weather_info = get_weather_info(code)
