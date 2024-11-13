467f92b4deec2143cbb9ff32e838cfe1b4cfd89f
Daniel Martins <email.to.x@gmail.com>
2024-11-13 17:19:18 +0000

    Update replace_variables.py

--- replace_variables.py
+++ replace_variables.py
@@ -55,8 +55,8 @@ if __name__ == "__main__":
         'HOUR_+2': str(int(os.getenv('HOUR_NOW', 0))+2),
         '6AM_WEATHER_IMAGE':  get_weather_info(get_weather_response(5)[2])['image'],
         '6AM_WEATHER_CONDITIONS': get_weather_info(get_weather_response(5)[2])['description'],
-        '6AM_RAIN_FORECAST': get_weather_response(5)[1],
-        '6AM_RAIN_PROBABILITY ': get_weather_response(5)[0]
+        '6AM_RAIN_FORECAST': str(get_weather_response(5)[1]),
+        '6AM_RAIN_PROBABILITY ': str(get_weather_response(5)[0])
         
         # Add more variables as needed
     }
